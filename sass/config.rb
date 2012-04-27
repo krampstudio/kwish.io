3337bc101167864016b4c7ffc79baffa068d05df
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-04-27 12:54:28 +0200

    Change sass directory

--- sass/config.rb
+++ sass/config.rb
@@ -2,13 +2,14 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "stylesheets/sass"
-images_dir = "imgs"
-javascripts_dir = "scripts"
+css_dir = "../public/stylesheets"
+sass_dir = "."
+images_dir = "../public/imgs"
+javascripts_dir = "../public/scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
