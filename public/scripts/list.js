c28007d17432602eb2f39f8e0032b0dbfb20a519
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-08-05 15:33:05 +0200

    Enable to add and remove list items (client side)

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -16,35 +16,52 @@ $(document).ready(function(){
             $('#header').text(list.title);
             $('#intro').html('<p>'+list.description+'</p>');   
   
-           $('.editable').editableArea({
+           $('.editable').not('#items').editableArea({
                 hoverClass : 'editable-hover'
            });
            
            $.post('/list/articles', {list: list}, function(articles){
-            var $list = $('#items > ul');
-            for (var index in articles){
-                var article = articles[index];
-                var shortDesc = article.description; 
-                if(article.description.length > 100){
-                    shortDesc = shortDesc.substring(0,95) + '[...]';                    
-                }
-                 
-                $list.append("<li>\
-                                <div class='item-title'>"+article.name+"</div>\
-                                <img class='item-thumbnail' src='/imgs/articles/"+article.thumb+"' />\
-                                <div class='item-desc'>"+shortDesc+"</div>\
-                             </li>");
-            }
-             $list.append("<li class='ctrl-box'></li>");
-             
-             $list.sortable({
-                cursor: 'move',
-                forcePlaceholderSize: true,
-                opacity: 0.6,
-                placeholder: 'placeholder',
-                items: 'li:not(.ctrl-box)'          //to exclude the control box
-           });
-             
+				var $list = $('#items > ul');
+				for (var index in articles){
+					var article = articles[index];
+					var shortDesc = article.description; 
+					if(article.description.length > 100){
+						shortDesc = shortDesc.substring(0,95) + '[...]';                    
+					}
+						 
+					$list.append("\
+							<li>\
+								<div class='item-title'>"+article.name+"</div>\
+								<img class='item-thumbnail' src='/imgs/articles/"+article.thumb+"' alt='' />\
+								<div class='item-desc'>"+shortDesc+"</div>\
+                           	</li>\
+							");
+            	}
+            	if($('#items').hasClass('editable')){
+					//enable to add an article
+					$list.append("\
+								<li class='ctrl-box'>\
+									<img class='icon' src='/imgs/add.png' alt='+' />\
+								 	Ajouter un article \
+								 </li>\
+								");
+					//add a new item by clicking the ctrl button
+					$('.ctrl-box', $list).click(function(){
+						$('<li/>').insertBefore($(this));
+					});
+					
+
+					//the articles can be sorted 					
+					$list.sortable({
+						cursor: 'move',
+						forcePlaceholderSize: true,
+						opacity: 0.6,
+						placeholder: 'placeholder',
+						items: 'li:not(.ctrl-box)'          //to exclude the control box
+					});
+
+					$('li', $list).removableArea();
+             	}
            }, 'json');
            
            
