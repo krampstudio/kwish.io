a4d7bb09c167c669c81f1f22332e954362350926
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-11-27 23:11:53 +0100

    implement list id management

--- test/klistProvider_test.js
+++ test/klistProvider_test.js
@@ -6,17 +6,25 @@ exports.klistProviderTest = {
 
     setUp : function(done){
         this.login = 'krampstudio';
+        this.id = 'foolist';
+        this.list = {
+            title : 'Awesome Foo List',
+            desc : 'Really awesome list',
+            state: 'public'
+        };
         done();
     },
 
     testProviderStruct : function(test){
-        test.expect(3);
+        test.expect(5);
 
         var klistProvider = require('../lib/providers/klist');
 
         test.ok(typeof klistProvider !== undefined);
         test.ok(typeof klistProvider.getUserLists  === 'function');
-        test.ok(typeof klistProvider.add  === 'function');
+        test.ok(typeof klistProvider.save  === 'function');
+        test.ok(typeof klistProvider.del  === 'function');
+        test.ok(typeof klistProvider.getId  === 'function');
 
         test.done();
     },
@@ -31,6 +39,48 @@ exports.klistProviderTest = {
             test.strictEqual(err, null);
             test.equal(lists.length, 3);
             test.ok(lists.indexOf('ditasbirth') > 0);
+
+            test.done();
+        });
+    },
+
+    testSaveList : function(test){
+        test.expect(2);
+
+        var self = this;
+        var klistProvider = require('../lib/providers/klist');
+
+        klistProvider.save(this.login, this.id, this.list,  function(err, saved){
+            test.strictEqual(err, null);
+            test.strictEqual(saved, true);
+            
+            test.done();
+        });
+    },
+    
+    testGetId : function(test){
+        test.expect(2);
+
+        var self = this;
+        var klistProvider = require('../lib/providers/klist');
+        klistProvider.getId('Foo List',  function(err, id){
+            test.strictEqual(err, null);
+            test.equal(id, 'foo-list');
+            
+            test.done();
+        });
+    },
+
+    testDelList : function(test){
+        test.expect(2);
+
+        var self = this;
+        var klistProvider = require('../lib/providers/klist');
+
+        klistProvider.del(this.login, this.id, function(err, removed){
+            test.strictEqual(err, null);
+            test.strictEqual(removed, true);
+            
             client.quit();
             test.done();
         });
