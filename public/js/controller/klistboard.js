6ca72d0f75780db122dace4df52d08a0f63d1c84
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-01-02 00:15:29 +0100

    nav bar login/logout

---
+++ public/js/controller/klistboard.js
@@ -0,0 +1,12 @@
+define([], function(){
+    'use strict';
+
+    var klistboardController = {
+        dispatch : function(){
+
+        }
+    };
+
+    return klistboardController;
+
+});
--- public/js/controller/login.js
+++ public/js/controller/login.js
@@ -1,5 +1,8 @@
-define(['jquery', 'lodash', 'ui/nav', 'ui/form'], function($, _, Nav, Form){
+define(['jquery', 'lodash', 'ui/nav', 'ui/form'], function($, _, nav, form){
     'use strict';
+
+    var $loginMenu = $('.login-menu');
+    var $logoutMenu = $('.logout-menu');
 
     var loginController = {
 
@@ -8,18 +11,36 @@ define(['jquery', 'lodash', 'ui/nav', 'ui/form'], function($, _, Nav, Form){
         },
         
         _setUpLoginForm : function(){
+            var self = this;
             var $loginForm = $('#login-form');
-            Form.setUp($loginForm, this._login);
+            form.setUp($loginForm, function(data){
+                self._login(data);
+            });
         },
 
         _login : function(data){
+            var self = this;
             $.post('/auth-local', data, function(res){
                 if(res.auth && res.auth === true){
-                    Nav.open('klistboard', false);
+                    self._updateNavBar(data.login);
+                    nav.open('klistboard', false);
                 } else {
+                    self._updateNavBar(false);
                     alert('wrong');
                 }
             }, 'json');
+        },
+
+        _updateNavBar : function(login){
+            if(login){
+                $loginMenu.hide();        
+                $logoutMenu.show(); 
+                $logoutMenu.find('li:first-child > a').text(login);       
+            } else {
+                $loginMenu.show(); 
+                $logoutMenu.hide(); 
+                $logoutMenu.find('li:first-child > a').text('');       
+            }
         }
     };
 
