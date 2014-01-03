5fc844e4ff4cf126fd856d4f4860c71153effa92
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-01-03 15:30:03 +0100

    Create session based client side storage

---
+++ public/js/ui/session.js
@@ -0,0 +1,35 @@
+define(['modernizr', 'jquery'], function(Modernizr, $){
+    'use strict';    
+
+    var prefix = 'kwish:';
+
+    var store;
+
+    if(Modernizr.sessionstorage){
+        store = window.sessionStorage;
+    } else {
+        $.error('Implement cookie store');
+    }
+
+    var Session = {
+        
+        get : function(key){
+            
+            return store.getItem(prefix + key);
+        },
+
+        set : function(key, value){
+            store.setItem(prefix + key, value);
+        },
+
+        isset : function(key){
+            return this.get(prefix + key) !== null; 
+        },
+
+        rm : function(key){
+            store.removeItem(prefix + key);
+        }
+    };
+
+    return Session;
+});
