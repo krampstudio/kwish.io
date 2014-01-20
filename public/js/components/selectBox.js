88c7957edefe6c62be5b22d2bf6f97b04007bd10
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-01-20 22:20:47 +0100

    add select box to the board

--- public/js/components/selectBox.js
+++ public/js/components/selectBox.js
@@ -4,35 +4,21 @@ define(['jquery', 'lodash'], function($, _){
     var itemTmpl = _.template(
         '<div class="selectbox-item selectbox-${state}">' +
             '<a href="#">${value}</a>' +
-            '<i class="fa fa-delete" />' +
+            '<span class="remover"></span>' +
         '</div>'
     );
     var itemAddTmpl = _.template(
         '<div class="selectbox-item adder">' +
-            '<i class="fa fa-add" /> Add' +
+            '<a href="#">Add</a>' +
         '</div>'
     );
 
     var states = ['selectable', 'selected', 'disabled'];
+
     var defaults = {
-        items : [
-            {id: '1', value: 'name 1', state: 'selected'},
-            {id: '2', value: 'name 2', state: 'selectable'},
-            {id: '3', value: 'name 3', state: 'selectable'}
-        ],
-        add : function(cb){
-            cb({id: '3', value: 'name 3', state: 'selectable'});
-        },
-        added : function(e, item, $itemElt){
-            console.log("added", arguments);
-        },
-        remove : true,
-        removed : function(e, item){
-            console.log("moveded", arguments);
-        },
-        selected : function(e, item, $itemElt){
-            console.log("selected", arguments);
-        }
+        items : [],
+        add : false,
+        remove : false
     };
 
     /**
@@ -43,13 +29,20 @@ define(['jquery', 'lodash'], function($, _){
      */
     var selectBox = {
 
+
         /**
          * Plugin initialisation
          * @public
+         * @param {Object} options - the plugin options
+         * @param {Array} options.items - the item list to be displayed, each item must have a value and a state property
+         * @param {AddCallback|Boolean} [options.add = false] - the function used to add a new item
+         * @param {RmCallback|Boolean} [options.remove = false] -  the function used to remove item
          * @returns {jQueryElement} for chaining
+         * @fires selectBox#create.selectBox
          */
         init : function(options){
             var self = selectBox;
+            
             options = _.defaults(options || {}, defaults);
 
             return this.each(function(){
@@ -58,48 +51,99 @@ define(['jquery', 'lodash'], function($, _){
 
                     $elt.data('selectBox', options);
 
+                    /**
+                     * This function adds an item to the selectBox
+                     * @param {Object} item - the item to add
+                     */
                     var addItem = function addItem(item){
+                        
+                        //add the element
                         var $itemElt = self._add($elt, item);
+                        
+                        //bind select item
                         $itemElt.on('click', function(e){
                             e.preventDefault();
-                            self._select($elt, $(this));
-                        });          
-                        if(options.remove === true){
-                            $('.fa-delete', $itemElt).on('click', function(e){
+                            self._select($elt, $itemElt);
+                        });  
+        
+                        //bind removal
+                        if(_.isFunction(options.remove)){
+                            $('.remover', $itemElt).on('click', function(e){
                                 e.preventDefault();
-                                self._remove($elt, $(this));
+                                
+                                /**
+                                 * Called back by the defined function to remove an item
+                                 * @callback RmCallback
+                                 * @param  {Function} cb - the callback that will get the confirmed boolean in parameter
+                                 */
+                                options.remove.call($elt, function(confirmed){
+                                    if(confirmed === true){
+                                        self._remove($elt, $itemElt);
+                                    }
+                                });
                             });
+                        } else {
+                            $('.remover', $itemElt).remove();
+                        }
+                        
+                        //if the state is set to selected
+                        if(item.state === 'selected'){
+                            self._select($elt, $itemElt);
                         }
                     };
 
+                    //add defined items
                     _.forEach(options.items, addItem);
          
+                    //add the adder
                     if(_.isFunction(options.add)){
                         var $adder = $(itemAddTmpl()).appendTo($elt);
                         $adder.click(function(e){
                             e.preventDefault();
+                            
+                            /**
+                             * Called back by the defined function to add an item
+                             * @callback AddCallback
+                             * @param  {Function} cb - the callback that will get the item in parameter
+                             */
                             options.add.call($elt, addItem);                                                                        
                         });
                     }
 
                     /**
-                     * @event selectBox#.selectBox
+                     * The selectBox is created
+                     * @event selectBox#create.selectBox
                      */    
-                    $elt.on('added.selectBox', options.added);
-                    $elt.on('removed.selectBox', options.removed);
-                    $elt.on('selected.selectBox', options.selected);
-
                     $elt.trigger('create.selectBox');
                 }
             });
         },
 
+        /**
+         * Add an item to the selectBox
+         * @private
+         * @param {jQueryElement} $elt - the selectBox element
+         * @param {Object} item - the item to add   
+         * @returns {jQueryElement} $itemElt - the inserted element 
+         * @fires selectBox#added.selectBox
+         */
         _add : function($elt, item){
             var $itemElt = $(itemTmpl(item));
             $itemElt.data('selectbox-item', item);
             if($elt.children('.adder').length > 0){
-                var $items = $(".selectbox-item:not(.adder)");
-                $($items.get($items.length - 1)).after($itemElt);
+                if($elt.find('.selectbox-item').length > 0){
+                    var $items = $(".selectbox-item:not(.adder)");
+                    $($items.get($items.length - 1)).after($itemElt);
+                } else {
+                    $itemElt.appendTo($elt);
+                }
+                
+                /**
+                 * A new item has been added to the selectBox
+                 * @event selectBox#added.selectBox
+                 * @param {Object} item - the added item object
+                 * @param {jQueryElement} $itemElt - the added item element
+                 */    
                 $elt.trigger('added.selectBox', [item, $itemElt]);
             } else {
                 $itemElt.appendTo($elt);
@@ -107,16 +151,45 @@ define(['jquery', 'lodash'], function($, _){
             return $itemElt;
         },
 
+        /**
+         * Select an item (change it's state/css class)
+         * @private
+         * @param {jQueryElement} $elt - the selectBox element
+         * @param {jQueryElement} $itemElt - the item element
+         * @fires selectBox#added.selectBox
+         */
         _select : function($elt, $itemElt){
+        
+            //toggle css classes
             $('.selectbox-item', $elt).removeClass('selectbox-selected').addClass('selectbox-selectable');
             $itemElt.removeClass('selectbox-selectable').addClass('selectbox-selected');
+            
+            /**
+             * An item has been marked as selected
+             * @event selectBox#selected.selectBox
+             * @param {Object} item - the selected item object
+             * @param {jQueryElement} $itemElt - the selected item element
+             */    
             $elt.trigger('selected.selectBox', [$itemElt.data('selectbox-item'), $itemElt]);
         },
 
+        /**
+         * Remove an item 
+         * @private
+         * @param {jQueryElement} $elt - the selectBox element
+         * @param {jQueryElement} $itemElt - the item element
+         * @fires selectBox#removed.selectBox
+         */
         _remove : function($elt, $itemElt){
             var item = $itemElt.data('selectbox-item');
             $itemElt.remove();
-            $elt.trigger('remove.selectBox', [item]);
+            
+             /**
+             * An item has been removed
+             * @event selectBox#selected.selectBox
+             * @param {Object} item - the removed item object
+             */    
+            $elt.trigger('removed.selectBox', [item]);
         },
             
         /**
--- public/js/controller/klistboard.js
+++ public/js/controller/klistboard.js
@@ -1,19 +1,43 @@
-define(['jquery', 'core/nav', 'components/selectBox'], function($, nav){
+define(['jquery', 'lodash', 'core/nav', 'core/session', 'components/selectBox'], function($, _, nav, session){
     'use strict';
 
     var currentList;
 
-
-
     var klistboardController = {
         
         dispatch : function(){
-           
-//           this.getList('ditasbirth');
+            var self = this;
+            var $klistsBox = $('#klist-selector');
+            var current = session.isset('klist') ? session.get('klist') : false;
+            
             this.getLists(function gotLists(lists){
-                $('#klist-selector').selectBox();
+            
+                var items = _.map(lists, function(list, index){
+                    return {
+                        id : index,
+                        value: list,
+                        state : (current === list || (current === false && index === 0)) ? 'selected' : 'selectable' 
+                    };
+                });                
+                 
+                $klistsBox.on('selected.selectBox', function(e, item){
+                    self.getList(item.value);                   
+                });
+                $klistsBox.selectBox({
+                    items : items,
+                    add : function(cb){
+                        var size =  $('.selectbox-item').length - 1;
+                        return cb({
+                            id : size,
+                            value : 'Klist ' + size,
+                            state: 'selectable'
+                        });
+                    },
+                    remove : function(cb){
+                        return cb(true);
+                    }
+                });
             });
-
         },
 
         getLists : function(cb){
@@ -24,7 +48,8 @@ define(['jquery', 'core/nav', 'components/selectBox'], function($, nav){
         getList : function(name){
             var url = 'klist/' + name;
             nav.api(url, function(list){
-                console.log(list);
+                $('#klist').find("[data-bind='title']").text(list.title);
+                $('#klist').find("[data-bind='desc']").text(list.desc);
             });
         }
     };
