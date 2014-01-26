476b6c8c24dfc273fa4676510145da98f9318139
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-01-26 23:22:01 +0100

    start product advertising provider

---
+++ test/awsProvider_test.js
@@ -0,0 +1,22 @@
+var conf = require('../config/confLoader').init();
+var logger = require('../lib/logFactory').init();
+
+exports.userProviderTest = {
+
+    setUp : function(done){
+        done();
+    },
+
+    testAws : function(test){
+      var awsProvider = require('../lib/providers/aws');
+
+        awsProvider.search('rammstein Made In Germany', 'All', function(err, result){
+            test.strictEqual(err, null);
+            test.ok(result.count > 0);
+            test.ok(result.pages > 0);
+            test.ok(result.items.length > 0);
+            
+            test.done();
+        });
+    }
+};
